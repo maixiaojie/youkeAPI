9d6acabb3c5cb1d917b19fb8971fcc1bfe337b9f
maixiaojie <778273234@qq.com>
2019-04-02 10:29:16 +0800

    监听server

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const Hapi = require('hapi');
-
 let connnectionOptions = {
 	port: 7777,
 	host: 'localhost'
 };
 
 const server = new Hapi.server(connnectionOptions);
-
+// server.events.on({name: 'request', channels: 'error'}, fundebug.HapiErrorHandler);
+// server.events.on("response", fundebug.HapiErrorHandler);
 
 module.exports = server;
